118aa28502910961888cc0c0051f54f793496f49
io <senpai9464@yandex.ru>
2024-10-26 23:43:53 +0000

    end

--- core/main.go
+++ core/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"root/shared/middleware"
 	"root/shared/utils"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/ysugimoto/grpc-graphql-gateway/runtime"
 
 	auth "root/shared/proto/out"
@@ -14,19 +14,21 @@ import (
 func main() {
 	const port = ":6069"
 	log := utils.Logger()
-	app := fiber.New()
-	app.Use(logger.New())
-	mux := runtime.NewServeMux()
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
+	app.Use(middleware.Logger(log))
 
+	mux := runtime.NewServeMux()
 	if err := auth.RegisterAuthServiceGraphql(mux); err != nil {
-		log.WithError(err).Fatal("Failed to register GraphQL handler")
+		log.WithError(err).Fatal("❌ Failed to register GraphQL handler")
 		return
 	}
-
 	app.All("/graphql", adaptor.HTTPHandler(mux))
-
-	log.Infof("Listening on %s", port)
+	log.Infof("🌐 Server is running on %s", port)
 	if err := app.Listen(port); err != nil {
-		log.Fatal(err)
+		log.Fatal("❌ Server startup error:", err)
 	}
+
+	log.Info("✅ Server started successfully")
 }
