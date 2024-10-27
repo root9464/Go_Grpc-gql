1f4a53044d8e46c04688192280f7ada4f3713640
io <senpai9464@yandex.ru>
2024-10-27 00:19:11 +0000

    update scripts

--- scripts/db.sh
+++ scripts/db.sh
@@ -6,6 +6,15 @@ start_dbpsq() {
   docker-compose -f $DOCKERCOMPOSE_PATH up -d
 }
 
+stop_dbpsq() {
+  docker-compose -f $DOCKERCOMPOSE_PATH down
+}
+
+rm_dbpsq() {
+  docker rm postgres &&
+  docker rmi postgres
+}
+
 if [ $# -eq 1 ]; then
     "$1"
 fi
---
+++ scripts/packages.fix.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# в моменте у меня возникала проблема в том что не было пакета protoc-gen-go или protoc-gen-graphql
+# я очень долго потратил время на решение данной проблемы, и доконца не понял в чем были пиколы но 
+# данный скрипт создан мною для решения этой проблемы
+
+# ./packages.fix.sh pwhich protoc-gen-go     
+pwhich() {
+  [ $# -eq 1 ] && which "$1" || echo "Usage: pwhich <command>"
+}
+
+ppath() {
+  export PATH="$PATH:$(go env GOPATH)/bin"
+  echo "Updated PATH: $PATH"
+}
+
+pingo() {
+  echo "pong"
+}
+
+pinstall() {
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@latest &&
+  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest &&
+  go get github.com/ysugimoto/grpc-graphql-gateway/protoc-gen-graphql &&
+  go get github.com/graphql-go/graphql &&
+  go get github.com/ysugimoto/grpc-graphql-gateway/graphql &&
+  go get github.com/ysugimoto/grpc-graphql-gateway
+}
+
+# Main script logic
+case "$1" in
+    ppath)
+        ppath
+        ;;
+    pingo)
+        pingo
+        ;;
+    pwhich)
+        [ $# -eq 2 ] && pwhich "$2" || echo "Usage: pwhich <command>"
+        ;;
+    *)
+        echo "Invalid command: $1"
+        echo "Usage: $0 {pwhich|ppath|pingo} [args]"
+        exit 1
+        ;;
+esac
